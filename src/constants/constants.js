efdd87b8a234aaef2631e5dbe87a5b1e57bd57b7
Malith Kuruppu <info.mkappworks@gmail.com>
2021-08-07 21:20:54 +0530

    Updated color param

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const firstReq = {
-  // https://cataas.com/cat/says/Hi%20There?width=500&amp;height=800&amp;c=Cyan&amp;s=150
+  // https://cataas.com/cat/says/Hi%20There?width=500&height=800&c=Cyan&s=150
   url:
     "https://cataas.com/cat/says/" +
     process.env.CAT_GREETING +
@@ -9,7 +9,7 @@ const firstReq = {
     process.env.CAT_WIDTH +
     "&height=" +
     process.env.CAT_HEIGHT +
-    "&color" +
+    "&color=" +
     process.env.CAT_COLOR +
     "&s=" +
     process.env.CAT_SIZE,
@@ -24,7 +24,7 @@ const secondReq = {
     process.env.CAT_WIDTH +
     "&height=" +
     process.env.CAT_HEIGHT +
-    "&color" +
+    "&color=" +
     process.env.CAT_COLOR +
     "&s=" +
     process.env.CAT_SIZE,
