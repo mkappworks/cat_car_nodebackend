df19cdfe704e35e4350b3a8910caed56761754c7
Malith Kuruppu <info.mkappworks@gmail.com>
2021-08-07 21:11:03 +0530

    Updated image format to jpg

--- src/controller/helper/file_write_helper.js
+++ src/controller/helper/file_write_helper.js
@@ -1,8 +1,8 @@
 const asyncHandler = require("express-async-handler");
 
-//helper function to write image in to file as cat-card and .png format
+//helper function to write image in to file as cat-card and .jpg format
 const fileWriteHelper = asyncHandler(async (image) =>
-  image.write("cat-card.png", () => console.log("The file was saved!"))
+  image.write("cat-card.jpg", () => console.log("The file was saved!"))
 );
 
 module.exports = fileWriteHelper;
